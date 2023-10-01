3db01a5305334adcbc31879681fc18039fcbc742
ifouzezi <ifoghaleuzezi@gmail.com>
2023-10-01 12:49:27 +0100

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
-export default function concatenateArrays(firstArray, secondArray, stringArray) {
-    return [...firstArray, ...secondArray, ...stringArray];
+export default function concatArrays(array1, array2, string) {
+    return [...array1, ...array2, ...string];
   }
-  
